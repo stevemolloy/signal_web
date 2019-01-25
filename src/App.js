8714e9a4b777afc453328f7418aedeb5afa4d7c5
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-01-25 13:37:03 +0100

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../public/plc_data.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('public/plc_data.json')
+      .then((json_obj) => console.log(json_obj))
+      .catch((err) => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. Hello!
           </p>
           <a
             className="App-link"
