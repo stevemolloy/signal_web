68fbe3e89b25ab101d193c48671fee8df91a2a7c
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-01-29 13:59:17 +0100

    Tidy up

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,25 +4,21 @@ const header = (props) =>  {
   let breadcrumbItems = [];
   if (props.breadcrumbs.length > 0) {
     breadcrumbItems = props.breadcrumbs.map((element, index) =>
-      <li
-        onClick={(e) => props.breadcrumbClickHandler(index, e)}
-        key={index}
-        >
+      <li onClick={(e) => props.breadcrumbClickHandler(index, e)}
+        key={index} >
         {element}
       </li>
     );
   }
 
   breadcrumbItems.unshift(
-    <li
-      onClick={(e) => props.breadcrumbClickHandler(-1, e)}
-      key={-1}
-      >
+    <li onClick={(e) => props.breadcrumbClickHandler(-1, e)}
+      key={-1} >
       Top
     </li>
   )
 
-  let breadcrumbList = <></>;
+  let breadcrumbList = <p> </p>;
   if (props.breadcrumbs.length>0) {
     breadcrumbList = (
       <ul className="breadcrumbs-list">
